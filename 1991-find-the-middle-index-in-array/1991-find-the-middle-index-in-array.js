b473214526d9af7af78a90af81da3a353e74c47c
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-07-27 22:58:51 +0530

    Time: 110 ms (5.17%), Space: 55.7 MB (5.17%) - LeetHub

---
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMiddleIndex = function(nums) {
+    // let me check for all the array elements
+    // check from start to end
+    let sum = nums.reduce((acc,current)=>{
+        return acc+ current;
+    })
+    let rsum = sum- nums[0];
+    let lsum=0;
+    // this is lsum and rsum for the 0-th index element
+    for(let i=0;i<nums.length;i++){
+        console.log(lsum,rsum)
+        if (lsum==rsum){
+            return i;
+        }
+        lsum = lsum + nums[i];
+        rsum = rsum - nums[i+1];
+
+    }
+    return -1;
+    
+};
