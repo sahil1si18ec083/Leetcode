33e6119d8f2b8ed9db5b641e7ab6085b01ffcd59
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-08-07 02:51:18 +0530

    Time: 68 ms (54.48%), Space: 54.2 MB (26.7%) - LeetHub

---
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.js
@@ -0,0 +1,41 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {_Node} root
+ * @return {_Node}
+ */
+var connect = function(root) {
+     const queue=[]
+    if (root==null){
+        return null;
+    }
+    queue.push(root)
+    while(queue.length>0){
+        let queuelength = queue.length;
+        for(let i=0;i<queuelength;i++){
+            let temp = queue[0];
+            queue.shift();
+            // no need to populate next pointer for the extreme right node in every row
+            if (i!= queuelength-1){
+                temp.next = queue[0]
+            }
+            
+            if (temp.left!=null){
+                queue.push(temp.left)
+            }
+             if (temp.right!=null){
+                queue.push(temp.right)
+            }
+        }
+    }
+    return root
+    
+};
