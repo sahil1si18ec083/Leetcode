5de4e99bcea871408ebfe94b86b4500c34741405
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-08-16 22:04:50 +0530

    Time: 71 ms (41.41%), Space: 59.7 MB (16.06%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.js
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.js
@@ -13,22 +13,25 @@
  */
 var lowestCommonAncestor = function(root, p, q) {
     if (root==null){
-        return root;
+        return root
     }
     if (root.val ==p.val || root.val==q.val){
         return root
     }
-    let left = lowestCommonAncestor(root.left, p,q);
-    let right = lowestCommonAncestor(root.right, p,q);
-    
+
+    var left = lowestCommonAncestor(root.left, p,q)
+    var right = lowestCommonAncestor(root.right, p,q);
+    if (left!=null && right!=null){
+        return root;
+    }
     if (left==null){
-        return right
+        return right;
     }
     if (right==null){
         return left
     }
-    if (left!=null && right!=null){
-        return root
-    }
+
     return null
+
+    
 };
