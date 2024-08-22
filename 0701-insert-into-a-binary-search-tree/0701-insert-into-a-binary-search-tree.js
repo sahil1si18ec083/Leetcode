d19d368a9d5bb58ed274ccf9ab7b5d9f7d8a8c9f
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-08-23 02:08:01 +0530

    Time: 103 ms (20.93%), Space: 58.3 MB (86.75%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.js
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.js
@@ -12,9 +12,6 @@
  * @return {TreeNode}
  */
 var insertIntoBST = function (root, val) {
-    /*
-    Recursive Solution
-    
     if (root == null) {
         return new TreeNode(val)
     }
@@ -27,39 +24,5 @@ var insertIntoBST = function (root, val) {
 
     }
     return root
-     */
-    let temp = root;
-    if (root==null){
-        return new TreeNode(val);
-    }
-    while (temp != null) {
-        if (temp.left == null && temp.right == null) {
-            if (val < temp.val) {
-                temp.left = new TreeNode(val);
-                return root
-            }
-            else {
-                temp.right = new TreeNode(val);
-                return root
-            }
-        }
-        if (temp.right == null && val > temp.val) {
-            temp.right = new TreeNode(val);
-            return root
-        }
-        if (temp.left == null && val < temp.val) {
-            temp.left = new TreeNode(val);
-            return root
-        }
-        if (temp.val<val){
-            temp=temp.right
-
-        }
-        else{
-            temp= temp.left
-        }
-    }
-    return root
-
 
 };
