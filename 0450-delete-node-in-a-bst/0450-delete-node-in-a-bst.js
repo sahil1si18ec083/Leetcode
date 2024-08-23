6d4bc738c321b0817ae73373fa8593015849b891
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-08-23 23:14:45 +0530

    Time: 93 ms (10.37%), Space: 58.8 MB (83.47%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.js
@@ -0,0 +1,79 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+function search(root, key) {
+    if (root == null || root.val == key) {
+        return root
+    }
+    if (root.val > key) {
+        return search(root.left, key)
+    }
+    else {
+        return search(root.right, key)
+    }
+}
+function max(root) {
+    let val = -1000000;
+    while (root != null) {
+        val = Math.max(val, root.val);
+        root = root.right;
+    }
+    return val
+}
+function mydelete (root, key){
+    if (root == null) {
+        return root
+    }
+    if (root.val == key) {
+        // o child
+        if (root.left == null && root.right == null) {
+            return null;
+        }
+        // 1 child
+        if (root.left == null && root.right != null) {
+            return root.right
+        }
+        if (root.right == null && root.left != null) {
+            return root.left
+        }
+        // 2 childs
+        if (root.left != null && root.right != null) {
+            let maxleft = max(root.left);
+            root.val = maxleft;
+            root.left = mydelete (root.left, maxleft)
+        }
+
+    }
+    else if (root.val < key) {
+        root.right = mydelete (root.right, key)
+    }
+    else {
+        root.left = mydelete (root.left, key)
+    }
+    return root
+}
+var deleteNode = function (root, key) {
+
+
+    const node = search(root, key);
+    if (node == null) {
+        return root
+    }
+
+    let newroot = mydelete (root, key)
+
+    return newroot;
+
+
+
+};
