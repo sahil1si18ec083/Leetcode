3e672ea5e5436d591dcc9c4a5a25be444449f5c0
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-08-25 06:44:41 +0530

    Time: 150 ms (5.36%), Space: 59 MB (5.03%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
@@ -2,56 +2,54 @@
  * @param {string} s
  * @return {number}
  */
-var myAtoi = function(s) {
-    let result=0;
+var myAtoi = function (s) {
+    let result = 0;
     let signpositive = true;
-    if(s.length==0){
+    if (s.length == 0) {
         return 0;
     }
-    while(s[0]==" "){
-        s= s.slice(1)
+    while (s[0] == " ") {
+        s = s.slice(1)
     }
     console.log(s)
+    let j = 0;
 
-    for(let i=0;i<s.length;i++){
-        if (s[i]=="-"){
-            if (i==0){
-            signpositive= false
+    if (s[0] == "-") {
+        signpositive = false;
+
+        j++;
+    }
+    if (s[0] == "+") {
+
+
+        j++;
+    }
+    for (let i = j; i < s.length; i++) {
+
+
+        if (s.charCodeAt(i) >= 48 && s.charCodeAt(i) <= 57) {
+            console.log({ result })
+            result = result * 10 + (s.charCodeAt(i) - 48);
+            let resultwithsign = signpositive ? result : result * -1;
+            if (resultwithsign > Math.pow(2, 31) - 1) {
+                return Math.pow(2, 31) - 1
             }
-            else{
-               break
-            }
-        }
-        else if (s[i]=="0" && result==0 ){
-            continue;
-        }
-          else if (s[i]=="+"  ){
-            if (i!=0){
-                break
-            }
-            continue;
-        }
-        else if (s.charCodeAt(i)>=48 && s.charCodeAt(i)<=57 ){
-            
-            result = result * 10 + (s.charCodeAt(i)-48);
-            let resultwithsign = signpositive? result : result*-1;
-            if (resultwithsign>Math.pow(2,31)-1){
-                return Math.pow(2,31)-1
-            }
-            if (resultwithsign<-Math.pow(2,31)){
-                return -Math.pow(2,31)
+            if (resultwithsign < -Math.pow(2, 31)) {
+                return -Math.pow(2, 31)
 
             }
+
         }
-        else{
+        else {
+            console.log("h")
             break;
         }
 
     }
 
-   return (signpositive?result:result*-1)
+    return (signpositive ? result : result * -1)
 
 
 
-    
+
 };
