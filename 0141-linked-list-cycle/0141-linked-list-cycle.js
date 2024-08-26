0c000b94cfe174f7afe519c09a3f3d04d934780f
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-08-26 19:11:05 +0530

    Time: 63 ms (63.23%), Space: 54.8 MB (14.81%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -11,15 +11,15 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    let slow= head;
+    
+    let slow = head;
     let fast = head;
-    while(fast!=null && fast.next!=null){
-        slow= slow.next;
-        fast= fast.next.next;
-        if (slow==fast){
+    while(fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow== fast){
             return true
         }
     }
     return false
-    
 };
