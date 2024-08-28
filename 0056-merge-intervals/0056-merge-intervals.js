bbb6b691b7466c93734cd5deab1d7e3ba6109f3a
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-08-28 20:30:09 +0530

    Time: 89 ms (76.55%), Space: 58.1 MB (75.83%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -3,41 +3,19 @@
  * @return {number[][]}
  */
 var merge = function (intervals) {
-    function max(a, b) {
-        if (a >= b) {
-            return a;
-        }
-        else {
-            return b;
-        }
-    }
-    const top = [];
-    const bottom = [];
-    const result = [];
-    for (let i = 0; i < intervals.length; i++) {
-        top.push(intervals[i][0]);
-        bottom.push(intervals[i][1])
-    }
-    top.sort((a, b) => a - b);
-    bottom.sort((a, b) => a - b);
-
-
-    let temp = [top[0], bottom[0]]
+    intervals.sort((a, b) => a[0] - b[0])
+    const res = []
+    var temp = intervals[0];
     for (let i = 1; i < intervals.length; i++) {
-        if (top[i] <= temp[1]) {
-            temp = [temp[0], max(temp[1], bottom[i])]
-            console.log(temp)
+        if (intervals[i][0] >= temp[0] && intervals[i][0] <= temp[1] ) {
+            temp[1]= Math.max(temp[1],intervals[i][1])
+
         }
-        else {
-            result.push(temp);
-            temp = [top[i], bottom[i]]
+        else{
+            res.push(temp)
+            temp= intervals[i]
         }
     }
-    console.log(top)
-    console.log(bottom)
-    result.push(temp);
-    return result
-
-
-
+    res.push(temp)
+    return res
 };
