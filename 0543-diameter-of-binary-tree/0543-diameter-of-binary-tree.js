a55fdb20fb6d67e0e1ab90d5cdc4caf3787a083b
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-09-03 11:37:50 +0530

    Time: 675 ms (5.01%), Space: 57.5 MB (20.86%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
@@ -10,20 +10,21 @@
  * @param {TreeNode} root
  * @return {number}
  */
-function height(root) {
-    if (root == null) {
+
+function height(root){
+     if(root==null){
+        return 0
+     }
+     return (1+ Math.max(height(root.left), height(root.right)))
+} 
+var diameterOfBinaryTree = function(root) {
+    
+    if (root==null){
         return 0;
     }
-    return (1 + Math.max(height(root.left), height(root.right)))
-}
-var diameterOfBinaryTree = function (root) {
+    let leftdiameter = diameterOfBinaryTree(root.left)
+    let rightdiameter = diameterOfBinaryTree(root.right);
+    let diameter = height(root.left)+ height(root.right);
 
-    if (root == null) {
-        return 0;
-
-    }
-    let diamterofleft = diameterOfBinaryTree(root.left)
-    let diamterofright = diameterOfBinaryTree(root.right)
-    return (Math.max(diamterofleft, diamterofright, height(root.left) + height(root.right)))
-
+    return (Math.max(leftdiameter,rightdiameter,  diameter))
 };
