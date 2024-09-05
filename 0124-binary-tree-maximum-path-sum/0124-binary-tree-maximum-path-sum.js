97bb02b4bda4b355e56e37eac34d260049e3ff0c
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-09-05 15:07:44 +0530

    Time: 73 ms (33.22%), Space: 59.7 MB (9.84%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.js
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function (root) {
+    function solve(root, maxsum) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        var leftsum = solve(root.left, maxsum);
+        var rightsum = solve(root.right, maxsum);
+
+        if (leftsum >= 0 && rightsum >= 0) {
+            maxsum.value = Math.max(maxsum.value, root.val + leftsum + rightsum)
+            return (root.val + Math.max(leftsum, rightsum))
+        }
+        else if (leftsum < 0 && rightsum < 0) {
+            maxsum.value = Math.max(maxsum.value, root.val)
+            return root.val
+
+        }
+        else if (leftsum >= 0 && rightsum < 0) {
+             maxsum.value = Math.max(maxsum.value, root.val+leftsum )
+             return root.val+leftsum
+
+        }
+        else if (leftsum < 0 && rightsum >= 0) {
+            maxsum.value = Math.max(maxsum.value, root.val+rightsum )
+             return root.val+rightsum
+
+        }
+    }
+
+    const maxsum = {
+        value: -1001
+    }
+
+    solve(root, maxsum);
+
+    return maxsum.value
+
+
+};
