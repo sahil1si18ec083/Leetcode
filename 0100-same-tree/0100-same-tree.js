75787c60622d4a52906ecfcd3d78945d1949d732
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-09-08 04:25:04 +0530

    Time: 53 ms (55%), Space: 49.6 MB (10.19%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -11,13 +11,14 @@
  * @param {TreeNode} q
  * @return {boolean}
  */
-var isSameTree = function(p, q) {
-    if (p==null && q==null){
+var isSameTree = function (p, q) {
+    if (p == null && q == null) {
         return true;
     }
-    if (p==null || q==null){
-        return false;
+    if (p == null || q == null) {
+        return false
     }
-    return (p.val ==q.val &&  isSameTree(p.left,q.left) && isSameTree(p.right, q.right))
+
+    return (p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right))
     
 };
