bbc21ba8896577d30efacba3336cfe573a365290
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-09-10 12:13:24 +0530

    Time: 103 ms (43.39%), Space: 58.9 MB (16.4%) - LeetHub

---
+++ 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ function gcd(a,b){
+    if (a==0){
+        return b
+    }
+    if(b==0){
+        return a;
+    }
+    if (a>b){
+        return (gcd(a-b,b))
+    }
+    return  (gcd(a,b-a))
+ }
+var insertGreatestCommonDivisors = function(head) {
+     // base case for my recursion
+    if (head==null || head.next==null){
+        return head
+    }
+
+    var next = head.next;
+
+    var newNode = new ListNode(gcd(head.val, head.next.val));
+    head.next = newNode;
+    newNode.next = insertGreatestCommonDivisors(next); 
+    // simple recursive  code
+
+
+    return head
+
+    
+
+
+};
