c5077cd0b5c65d475ac7e9c99072361730d9186e
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-09-14 21:30:23 +0530

    Time: 74 ms (72.73%), Space: 59.9 MB (93.94%) - LeetHub

--- 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.js
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.js
@@ -3,20 +3,27 @@
  * @return {number}
  */
 var longestSubarray = function (nums) {
-    let max = nums[0]
-    for (let i = 1; i < nums.length; i++) {
-        max = Math.max(max, nums[i])
-    }
+    let max = nums[0];
     let maxlength = 1;
     let count = 0;
     for (let i = 0; i < nums.length; i++) {
+
         if (nums[i] == max) {
             count++;
+            maxlength = Math.max(count,maxlength);
         }
-        else {
-            count = 0;
+        else if (nums[i]>max){
+            max= nums[i];
+            count=1;
+            maxlength=1;
+
         }
-        maxlength = Math.max(count, maxlength);
+        else{
+            count=0;
+
+        }
+
+
     }
 
     return maxlength
