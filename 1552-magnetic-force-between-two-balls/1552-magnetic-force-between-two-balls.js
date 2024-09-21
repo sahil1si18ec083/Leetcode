abb768052e6f9b5ef823bc1b1040783559583a4f
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-09-22 04:59:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} position
+ * @param {number} m
+ * @return {number}
+ */
+var maxDistance = function (position, m) {
+
+    function isValid(position, m, mid){
+        let count=1;
+        let prev = position[0]
+        for(let i=1;i<position.length;i++){
+            if (position[i]-prev>=mid){
+                count++;
+                prev = position[i]
+            }
+
+        }
+        if(count==m){
+            return true
+        }
+        return false
+    }
+
+    position.sort((a, b) => a - b);
+
+    let low = 0;
+    let high = position[position.length - 1] - position[0];
+    let res;
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        if (isValid(position, m, mid)) {
+            low = mid + 1;
+            res = mid;
+        }
+        else {
+            high = mid - 1
+        }
+    }
+    return res;
+};
