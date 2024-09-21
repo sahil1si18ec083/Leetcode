244006030acb300582672bb2cb9b5cec4ef1c174
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-09-21 23:48:58 +0530

    Time: 48 ms (87.26%), Space: 48.5 MB (96.48%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.js
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.js
@@ -3,48 +3,43 @@
  * @param {number} k
  * @return {number}
  */
-var splitArray = function (nums, k) {
-    function isPossible(nums, k, mid) {
-        const n = nums.length;
-        let subarraycount = 0;
-        let sum = 0;
-        for (let i = 0; i < n; i++) {
-            if (nums[i] > mid) {
+var splitArray = function(nums, k) {
+    function ispossible(mid, nums, k){
+        let sum=0;
+        let c=1;
+        for(let i=0;i<nums.length;i++){
+            if (nums[i]>mid){
                 return false
             }
-            if (sum + nums[i] <= mid) {
-                sum = sum + nums[i]
-            }
-            else {
-                subarraycount++;
+            if(sum+nums[i]<=mid){
+                sum = sum + nums[i];
 
-                sum = nums[i]
             }
-        }
-        subarraycount++
-        if (subarraycount > k) {
-            return false
-        }
+            else{
+                c++;
+                sum = nums[i];
+                if(c>k){
+                    return false
+                }
+            }
 
+        }
         return true
     }
 
 
-    var low = Math.min(...nums)
-    var high = nums.reduce((acc, current) => acc + current, 0);
-    var ans;
-    while (low <= high) {
-        let mid = Math.floor((low + high) / 2);
-        console.log({ mid })
-        if (isPossible(nums, k, mid)) {
-            ans = mid;
-            high = mid - 1;
+    let low = Math.max(...nums);
+    let high = nums.reduce((acc,current)=>acc+current,0);
+    let res=-1;
+    while(low<=high){
+        let mid = Math.floor((low+high)/2);
+        if (ispossible(mid, nums, k)){
+            res = mid;
+            high = mid-1;
         }
-        else {
-            low = mid + 1;
+        else{
+            low = mid+1;
         }
     }
-
-    return ans;
-
+    return res;
 };
