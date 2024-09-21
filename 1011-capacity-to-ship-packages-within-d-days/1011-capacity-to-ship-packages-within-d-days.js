4863e304275dbce15e9f47b5fb44659ffff5324d
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-09-22 00:04:24 +0530

    Time: 73 ms (62.95%), Space: 53.4 MB (50.8%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.js
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.js
@@ -3,59 +3,40 @@
  * @param {number} days
  * @return {number}
  */
-const getMaxAndSum = (arr) => {
-    let max = arr[0];
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > max) {
-            max = arr[i]
-        }
-        sum = sum + arr[i]
-    }
-    return [max, sum];
-
-}
-function calculateDays(weights, cap) {
-  let days = 1; // First day.
-    let load = 0;
-    let n = weights.length; // size of array.
-    for (let i = 0; i < n; i++) {
-        if (load + weights[i] > cap) {
-            days += 1; // move to next day.
-            load = weights[i]; // load the weight.
-        } else {
-            // load the weight on the same day.
-            load += weights[i];
-        }
-    }
-    return days;
-
-
-
-
-}
 var shipWithinDays = function (weights, days) {
-    var maximum = Math.max(...weights)
+    function ispossible(mid, weights, days) {
+        let daycount=1;
+        let sum=0;
+        for(let i=0;i<weights.length;i++){
+            if (sum+weights[i]<=mid){
+                sum = sum + weights[i]
 
-    
+            }
+            else{
+                daycount++;
+                sum = weights[i]
+                if (daycount>days){
+                    return false
+                }
+            }
+        }
+        return true
 
-    let unsure;
-    let low = maximum;
-    let high =weights.reduce((acc,current)=> acc+current,0);
+    }
+
+    let low = Math.max(...weights);
+    let high = weights.reduce((acc, current) => acc + current, 0);
+    let res = -1;
     while (low <= high) {
-        let midCapacity = Math.floor((low + high) / 2);
-        let daysCalculated = calculateDays(weights, midCapacity);
-        if (daysCalculated <= days) {
-            unsure = midCapacity;
-            high = midCapacity - 1;
-
+        let mid = Math.floor((low + high) / 2);
+        if (ispossible(mid, weights, days)) {
+            res = mid;
+            high = mid - 1;
         }
         else {
-            low = midCapacity + 1;
+            low = mid + 1;
         }
-
     }
-    return unsure;
-
+    return res;
 
 };
