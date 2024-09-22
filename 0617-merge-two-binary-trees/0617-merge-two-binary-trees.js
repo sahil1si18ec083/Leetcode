6f36a563553d79eb71a74fb2e37ed1ca4196df84
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-09-23 02:42:00 +0530

    Time: 77 ms (85.8%), Space: 57.6 MB (50.71%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.js
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.js
@@ -12,19 +12,15 @@
  * @return {TreeNode}
  */
 var mergeTrees = function(root1, root2) {
-    function solve(root1, root2){
-        if (root1==null){
-            return root2
-        }
-        if (root2==null){
-            return root1
-        }
-        const root= new TreeNode(root1.val + root2.val);
-        root.left = solve(root1.left, root2.left)
-        root.right = solve(root1.right, root2.right)
-        return root
+    if (root1==null){
+        return root2
     }
-
-    return solve(root1, root2)
+    if (root2==null){
+        return root1;
+    }
+    const root = new TreeNode(root1.val + root2.val);
+    root.left = mergeTrees(root1.left, root2.left)
+    root.right = mergeTrees(root1.right, root2.right)
+    return root
     
 };
