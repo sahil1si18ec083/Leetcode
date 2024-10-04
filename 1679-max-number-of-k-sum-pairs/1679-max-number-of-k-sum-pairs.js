fadfc041ce6109095d606df016ddb47cc4adff1b
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-10-04 22:25:02 +0530

    Time: 99 ms (82.1%), Space: 65.7 MB (20.82%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.js
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.js
@@ -5,47 +5,31 @@
  */
 var maxOperations = function (nums, k) {
 
-    // const map = new Map()
-    // for (let i = 0; i < nums.length; i++) {
-    //     if (!map.has(nums[i])) {
-    //         map.set(nums[i], 1)
-    //     }
-    //     else {
-    //         map.set(nums[i], map.get(nums[i]) + 1)
-    //     }
-    // }
-
-    // let res = 0;
-    // for (let i = 0; i < nums.length; i++) {
-    //     if (map.has(k - nums[i]) && map.get(k - nums[i]) > 0) {
-    //         if(k-nums[i]==nums[i]){
-
-    //         }
-    //         res++;
-    //         map.set(nums[i], map.get(nums[i]) - 1)
-    //         map.set(k - nums[i], map.get(k - nums[i]) - 1)
-    //     }
-    // }
-
-    // return res;
-    const n = nums.length;
-    let low=0;
-    let count=0;
-    let high = n-1;
-    nums.sort((a,b)=>a-b);
-    while(low<high){
-        if (nums[low]+nums[high]==k){
-            count++;
-            low++;
-            high--;
+    const map = new Map();
+    let res = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (!map.has(nums[i])) {
+            map.set(nums[i], 1)
         }
-        else if (nums[low]+ nums[high]<k){
-            low++;
-        }
-        else{
-            high--;
+        else {
+            map.set(nums[i], map.get(nums[i]) + 1)
         }
     }
-    return count;
+
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(k - nums[i]) && map.get(k - nums[i]) > 0) {
+            if ("map.has(nums[i]) && map.get(nums[i]) > 0") {
+                res++;
+
+            }
+
+
+            map.set(k - nums[i], map.get(k - nums[i]) - 1)
+        }
+    }
+
+    return Math.floor(res/2);
+
 
 };
