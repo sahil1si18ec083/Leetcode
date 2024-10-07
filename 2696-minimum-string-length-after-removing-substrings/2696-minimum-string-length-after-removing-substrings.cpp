ce5eafd781f00c08bfb4c8f390d0dbe0291f703a
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-10-07 18:28:10 +0530

    Time: 8 ms (42.58%), Space: 11.6 MB (31.24%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char> mystack;
+        mystack.push(s[0]);
+        for (int i = 1; i < s.size(); i++) {
+
+            if (mystack.empty()) {
+                mystack.push(s[i]);
+            } else {
+                if ((mystack.top()=='A' && s[i]=='B')||  (mystack.top()=='C' && s[i]=='D')) {
+                    mystack.pop();
+                } else {
+                    mystack.push(s[i]);
+                }
+            }
+        }
+        while (mystack.size() > 1) {
+            char top = mystack.top();
+            mystack.pop();
+            char secondtop = mystack.top();
+            if ((secondtop == 'A' && top == 'B') ||
+                (secondtop == 'C' && top == 'D')) {
+                mystack.pop();
+            } else {
+                mystack.push(top);
+                break;
+            }
+        }
+        return mystack.size();
+    }
+};
