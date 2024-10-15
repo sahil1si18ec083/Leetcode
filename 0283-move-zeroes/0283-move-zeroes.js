ffe4a52250c939cd802fde093d1387918420605f
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-10-15 14:13:10 +0530

    Time: 74 ms (65.43%), Space: 55.8 MB (5.3%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,21 +3,20 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    function swap(nums,i,j){
-        let temp = nums[i];
-        nums[i]= nums[j];
-        nums[j]= temp
-    }
+    let low=0;
     let mid=0;
-    let low=0; // low aise pointer hai jiskay left mai sirf non zeros hongay
-
-    while(mid<nums.length){
-        if (nums[mid]!=0){
-            swap(nums,mid,low);
-            low++
+    const n = nums.length;
+    while(mid<n){
+        if(nums[mid]!=0){
+            
+            [nums[mid], nums[low]]= [nums[low], nums[mid]];
+            low++;
+            mid++;
         }
-        mid++;
+        else{
+            mid++;
+        }
     }
-
-    return nums
+    return nums;
+    
 };
