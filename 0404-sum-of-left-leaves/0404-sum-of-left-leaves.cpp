09090e86010f5c415a715c077759a937dd887fce
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-10-23 19:17:28 +0530

    Time: 0 ms (100%), Space: 15.1 MB (62.58%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
@@ -6,39 +6,28 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
+     void solve(int &sum, TreeNode* root , bool isleft){
+        if (root==NULL){
+            return;
+        }
+        if (root->left==NULL && root->right==NULL && isleft){
+            sum += root->val;
+        }
+        solve(sum , root->left, true);
+        solve(sum , root->right, false);
+
+    }
     int sumOfLeftLeaves(TreeNode* root) {
-        vector<vector<int>> v;
-        queue<TreeNode*> q;
-        if (root == NULL) {
 
-            return 0;
-        }
-        int sum = 0;
-        q.push(root);
-        while (q.size() > 0) {
-            int size = q.size();
-
-            for (int i = 0; i < size; i++) {
-                TreeNode* temp = q.front();
-
-                q.pop();
-                if (temp->left != NULL) {
-                    q.push(temp->left);
-                    if (temp->left->left == NULL && temp->left->right == NULL) {
-                        sum += temp->left->val;
-                    }
-                }
-                if (temp->right != NULL) {
-                    q.push(temp->right);
-                }
-            }
-        }
-        return sum;
+        int sum =0;
+         bool isleft = false;
+         solve(sum , root, isleft);
+         return sum;
+        
     }
 };
