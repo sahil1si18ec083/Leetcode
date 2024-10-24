f3c0c452136f5db7f93e9c23a56b2432d4a0b9a9
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-10-24 15:06:55 +0530

    Time: 0 ms (100%), Space: 23.2 MB (24.51%) - LeetHub

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.cpp
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.cpp
@@ -16,7 +16,9 @@ public:
         if (root == NULL) {
             return NULL;
         }
-       
+       if (root->left == NULL && root->right == NULL && root->val == target) {
+            return NULL;
+        }
         root->left = removeLeafNodes(root->left, target);
         root->right = removeLeafNodes(root->right, target);
          if (root->left == NULL && root->right == NULL && root->val == target) {
