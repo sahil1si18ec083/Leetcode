55e55d7131dd4f5c2254f7ffc8c246fc7185730c
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-10-25 20:02:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -12,29 +12,71 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* root, vector<int>& inorder) {
-        if (root == NULL) {
-            return;
+    void solve(TreeNode* root, TreeNode* &prev, TreeNode* &first,
+               TreeNode* &second, TreeNode* &third) {
+        // we will apply normal morris traversal
+
+        TreeNode* current = root;
+        while (current != NULL) {
+            if (current->left == NULL) {
+                // push the value
+                if (current->val > prev->val) {
+                    prev = current;
+                } else {
+                    if (second == NULL) {
+                      
+                        second = current;
+                        first = prev;
+                        prev = current;
+                    } else {
+                        third = current;
+                        prev = current;
+                    }
+                }
+                current = current->right;
+
+            } else {
+                TreeNode* leftNode = current->left;
+                while (leftNode->right != NULL && leftNode->right != current) {
+                    leftNode = leftNode->right;
+                }
+                if (leftNode->right == current) {
+                    if (current->val > prev->val) {
+                        prev = current;
+                    } else {
+                        if (second == NULL) {
+                            second = current;
+                            first = prev;
+                            prev = current;
+                        } else {
+                            third = current;
+                            prev = current;
+                        }
+                    }
+
+                    leftNode->right = NULL;
+                    current = current->right;
+
+                } else {
+                    leftNode->right = current;
+                    current = current->left;
+                }
+            }
         }
-        solve(root->left, inorder);
-        inorder.push_back(root->val);
-        solve(root->right, inorder);
-    }
-    void recoverBST(TreeNode* root, vector<int>& inorder, int &index) {
-        if (root == NULL) {
-            return;
-        }
-        recoverBST(root->left, inorder, index);
-        root->val = inorder[index++];
-        recoverBST(root->right, inorder,index);
     }
     void recoverTree(TreeNode* root) {
-
-        vector<int> inorder;
-        solve(root, inorder);
-        sort(inorder.begin(), inorder.end());
-        int index =0;
-
-        recoverBST(root, inorder, index);
+        TreeNode* prev = new TreeNode(INT_MIN);
+        TreeNode* first = NULL;
+        TreeNode* second = NULL;
+        TreeNode* third = NULL;
+        // cout<<first;
+        // cout<<second;
+        // cout<<third;
+        solve(root, prev, first, second, third);
+        if (third == NULL) {
+            swap(first->val, second->val); // means swap karnay vale neighbours hai
+        } else {
+            swap(first->val, third->val);
+        }
     }
 };
