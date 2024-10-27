69d939cb2d506ea3378351f9d30238ec43189abf
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-10-27 05:52:16 +0530

    Time: 62 ms (8%), Space: 52.4 MB (41.18%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.js
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.js
@@ -11,24 +11,25 @@
  * @return {TreeNode}
  */
 var bstFromPreorder = function(preorder) {
-    function insert(root, val){
-        if(root==null){
-            return new TreeNode(val)
+    function solve(preorder, preorderIndex,min,max){
+        if (preorderIndex.val>=preorder.length ){
+            return null;
         }
-        if (val<root.val){
-            root.left = insert(root.left,val);
+        if (preorder[preorderIndex.val] <min ||  preorder[preorderIndex.val] >max){
+            return null
         }
-        else{
-            root.right = insert(root.right,val);
-        }
+        const root = new TreeNode(preorder[preorderIndex.val]);
+        preorderIndex.val = preorderIndex.val +1;
+        root.left = solve(preorder, preorderIndex,min,root.val);
+        root.right = solve(preorder, preorderIndex,root.val,max);
         return root
 
     }
-    const root = new TreeNode(preorder[0])
-    for(let i=1;i<preorder.length;i++){
-        insert(root, preorder[i])
-    }
 
-    return root
+
+    let preorderIndex={val:0}
+    let min=-10000;
+    let max=10000;
+    return solve(preorder, preorderIndex,min,max)
     
 };
