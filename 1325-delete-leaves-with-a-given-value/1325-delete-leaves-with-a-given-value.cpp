50a10c882a4664ae4ced030d00afba0b1e07bbe1
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-10-31 23:17:47 +0530

    Time: 0 ms (100%), Space: 23.1 MB (24.26%) - LeetHub

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.cpp
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.cpp
@@ -12,26 +12,16 @@
  */
 class Solution {
 public:
-    bool isleaf(TreeNode* root) {
-        if (root->left == NULL && root->right == NULL) {
-            return true;
-        }
-        return false;
-    }
     TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        // first check left child, then right child and then parent
+        // for this we need POST order traversal
         if (root == NULL) {
-            return root;
+            return NULL;
         }
+       
         root->left = removeLeafNodes(root->left, target);
         root->right = removeLeafNodes(root->right, target);
-        if (root->left && isleaf(root->left) && root->left->val == target) {
-            root->left = NULL;
-        }
-
-        if (root->right && isleaf(root->right) && root->right->val == target) {
-            root->right = NULL;
-        }
-        if (isleaf(root) && root->val == target) {
+        if (root->left == NULL && root->right == NULL && root->val == target) {
             return NULL;
         }
         return root;
