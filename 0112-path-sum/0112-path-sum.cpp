ef3711b27b7546988e09d0bb609e5477f31a24c2
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-11-03 02:56:42 +0530

    Time: 0 ms (100%), Space: 20.1 MB (6.49%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -12,22 +12,27 @@
  */
 class Solution {
 public:
-    bool findsum(int sum, int targetSum, TreeNode* root) {
-        
+    void solve(TreeNode* root, int targetSum, int sum, bool& flag) {
         if (root == NULL) {
-            return false;
+            return;
         }
-        if (root->left == NULL && root->right == NULL &&
-            sum + root->val == targetSum) {
-            return true;
+
+        if (root->left == NULL && root->right == NULL) {
+            if (sum + root->val == targetSum) {
+                flag = true;
+            }
+            return;
         }
-        bool l = findsum(sum + root->val, targetSum, root->left);
-        bool r = findsum(sum + root->val, targetSum, root->right);
-        return (l || r);
+        // left path explore karo
+        solve(root->left, targetSum, sum + root->val, flag);
+        // right path karo
+        solve(root->right, targetSum, sum + root->val, flag);
     }
     bool hasPathSum(TreeNode* root, int targetSum) {
-
+        bool flag = false;
         int sum = 0;
-        return findsum(sum, targetSum, root);
+        solve(root, targetSum, sum, flag);
+
+        return flag;
     }
 };
