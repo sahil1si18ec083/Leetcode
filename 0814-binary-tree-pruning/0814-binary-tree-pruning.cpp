bd8cb85f440ae87f4502b5356adc33ebd4acdc25
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-11-03 01:50:40 +0530

    Time: 0 ms (100%), Space: 12.1 MB (12.35%) - LeetHub

--- 0814-binary-tree-pruning/0814-binary-tree-pruning.cpp
+++ 0814-binary-tree-pruning/0814-binary-tree-pruning.cpp
@@ -16,8 +16,8 @@ public:
             return NULL;
         }
         root->left = pruneTree(root->left);
-        root->right  = pruneTree(root->right);
-        if(root->left==NULL && root->right==NULL  && root->val==0){
+        root->right = pruneTree(root->right);
+        if(root->left==NULL  && root->right==NULL && root->val ==0){
             return NULL;
         }
         return root;
