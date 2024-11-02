076e2003e8ebcebbd6918ac0b8ec1d93454c7357
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-11-03 04:31:58 +0530

    Time: 0 ms (100%), Space: 25.7 MB (38%) - LeetHub

---
+++ 0623-add-one-row-to-tree/0623-add-one-row-to-tree.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+         if(depth==1){
+            TreeNode* newroot = new TreeNode(val);
+            newroot->left= root;
+            return newroot;
+         }
+        int currentdepth = 1;
+        queue<TreeNode*> q;
+        q.push(root);
+       
+
+        while (q.size() > 0) {
+            int qsize = q.size();
+
+            for (int i = 0; i < qsize; i++) {
+                TreeNode* temp = q.front();
+                q.pop();
+                if (currentdepth == depth - 1) {
+                    TreeNode* leftNode = temp->left == NULL ? NULL : temp->left;
+                    TreeNode* rightNode =
+                        temp->right == NULL ? NULL : temp->right;
+                    TreeNode* first = new TreeNode(val);
+                    TreeNode* second = new TreeNode(val);
+                    temp->left = first;
+                    temp->right = second;
+                    first->left = leftNode;
+                    second->right = rightNode;
+                } else {
+                    if (temp->left) {
+                        q.push(temp->left);
+                    }
+                    if (temp->right) {
+                        q.push(temp->right);
+                    }
+                }
+            }
+
+            currentdepth++;
+        }
+        return root;
+    }
+};
