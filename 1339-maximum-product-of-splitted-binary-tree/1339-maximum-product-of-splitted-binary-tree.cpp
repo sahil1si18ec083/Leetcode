a6c0df09d150e048cc6b71087c387a8b6927ae1b
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-11-03 19:17:14 +0530

    Time: 0 ms (100%), Space: 76.6 MB (49.12%) - LeetHub

---
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    long long int MOD = 1000000007;
+    long long int sum(TreeNode* root) {
+        if (root == NULL)
+            return 0;
+        int lsum = sum(root->left) ;
+        int rsum = sum(root->right) ;
+        return (root->val  + lsum + rsum);
+    }
+    int dfs(TreeNode* root, int totalsum, long long int& maxproduct) {
+        if (root == NULL) {
+            return 0;
+        }
+        long long int lsum = dfs(root->left, totalsum, maxproduct) ;
+        long long int rsum = dfs(root->right, totalsum, maxproduct) ;
+        long long int product1 = (root->val % MOD + lsum + rsum) ;
+        long long int product2 = (totalsum  - product1  ) ;
+        long long int totalproduct = (product1 * product2 ) ;
+        maxproduct = max(maxproduct, totalproduct);
+        return (root->val  + lsum + rsum) ;
+    }
+    int maxProduct(TreeNode* root) {
+        int totalsum = sum(root);
+
+        long long int maxproduct = 0;
+        long long int val = dfs(root, totalsum, maxproduct);
+        return maxproduct % MOD;
+    }
+};
