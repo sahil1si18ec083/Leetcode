9df8ad8daf0f8d87a928229ade690baf4a0df2f8
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-11-04 00:59:25 +0530

    Time: 313 ms (13.16%), Space: 86.2 MB (29.11%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.js
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.js
@@ -3,40 +3,36 @@
  * @param {number} k
  * @return {number[]}
  */
-var getAverages = function(nums, k) {
-    // first calculate the sum of  the first window
-    var sum=0;
+var getAverages = function (nums, k) {
+    const res = []
     const n = nums.length;
-    const res=[]
-    if (k>=n || n<=2*k){
-        for(let i=0;i<n;i++){
-            res.push(-1)
-        }
-        return res;
+    console.log(n)
+    if (n <= 2*k) {
+        return new Array(n).fill(-1)
     }
-    for (let i=0;i<k;i++){
-        res.push(-1);
+    for (let i = 0; i < k; i++) {
+        res.push(-1)
     }
-    for (let i=0;i<=2*k;i++){
-        sum = sum + nums[i]
-    }
-    console.log({sum})
-    let i=0;
-    let j=2*k;
-    
-     res.push(Math.floor(sum/(2*k+1)));
-    while(j<n-1){
 
-        sum = sum-nums[i];
+    let sum = 0;
+    for (let it = 0; it <= 2 * k; it++) {
+        sum = sum + nums[it]
+    }
+    res.push(Math.floor(sum / (2 * k + 1)))
+    let i = 0;
+    let j = 2 * k;
+
+    while (j < n - 1) {
         j++;
         sum = sum + nums[j];
+        sum = sum - nums[i];
         i++;
-        res.push(Math.floor(sum/(2*k+1)));
+        res.push(Math.floor(sum / (2 * k + 1)))
+    }
+    for (let i = 0; i < k; i++) {
+        res.push(-1)
+    }
+    return res
 
-    }
-      for (let i=0;i<k;i++){
-        res.push(-1);
-    }
-    return res;
-    console.log(res)
+
 };
