f8c0ad32587f9973bff4e13a8e0c92b1ba634021
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-11-04 16:00:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3163-string-compression-iii/3163-string-compression-iii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string compressedString(string word) {
+        string comp="";
+        char ch= word[0];
+        int n = word.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(word[i]==ch){
+                count++;
+            }
+            else{
+
+                int rem = count%9;
+                int factor = count/9;
+                if(factor>0){
+                    int n = 9;
+                    for(int i=1;i<=factor;i++){
+                        comp= comp + to_string(n)+ch;
+                    }
+                }
+                if(rem>0){
+                    comp= comp + to_string(rem)+ch;
+                }
+                ch= word[i];
+                count=1;
+            }
+        }
+        if(count>0){
+            int rem = count%9;
+                int factor = count/9;
+                if(factor>0){
+                    int n = 9;
+                    for(int i=1;i<=factor;i++){
+                        comp= comp + to_string(n)+ch;
+                    }
+                }
+                if(rem>0){
+                    comp= comp + to_string(rem)+ch;
+                }
+        }
+        return comp;
+        
+    }
+};
