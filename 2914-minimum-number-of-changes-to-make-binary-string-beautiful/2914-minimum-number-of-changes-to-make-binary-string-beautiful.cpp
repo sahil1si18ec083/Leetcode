2047e0862f1366b10129769d7104d07e554ca9ad
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-11-05 12:15:56 +0530

    Time: 0 ms (100%), Space: 12.2 MB (16.88%) - LeetHub

--- 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
     int minChanges(string s) {
-        int minchnages = INT_MAX;
-        int count=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='0'){
+        int count=1;
+        int n = s.size();
+        int correction=0;
+        int i=1;
+
+        while(i<n){
+            if(s[i]==s[i-1]){
                 count++;
+                i++;
             }
-        }
-        minchnages= min(minchnages , count);
-        count=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='1'){
-                count++;
+            else{
+                if(count%2==1){
+                    // count odd hai then we need a coreection
+                    correction++;
+                    i=i+2;
+                    count=1;
+                }
+                else{
+                    i=i+1;
+                    count=1;
+
+                }
+
+
             }
+
         }
-        minchnages= min(minchnages , count);
-        return minchnages;
+        return correction;
+        
     }
 };
