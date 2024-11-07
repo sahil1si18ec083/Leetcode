dc03f63c3fb6d551403ef322c340433f54cb910c
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-11-08 02:40:22 +0530

    Time: 0 ms (100%), Space: 17.6 MB (26.67%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -6,27 +6,27 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    bool solve(TreeNode* l, TreeNode* r) {
-        if (l == NULL && r == NULL) {
+    bool solve(TreeNode* l, TreeNode* r){
+        if(l==NULL && r==NULL){
             return true;
         }
-        if (l == NULL || r == NULL) {
+        if(l==NULL ||  r==NULL){
             return false;
         }
-        return (l->val == r->val && solve(l->left, r->right) &&
-                solve(l->right, r->left));
+        return (l->val ==r->val && solve(l->left, r->right) && solve(l->right, r->left));
     }
     bool isSymmetric(TreeNode* root) {
-        // Write your code here
-        if (root == NULL) {
+        if (root==NULL){
             return true;
         }
-        return solve(root->left, root->right);
+        TreeNode* l = root->left;
+        TreeNode* r = root->right;
+        return solve(l,r);
+        
     }
 };
