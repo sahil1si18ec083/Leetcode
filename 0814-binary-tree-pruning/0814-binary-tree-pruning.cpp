f99d93c09dd1e5061cddf73056c6befb259aeda9
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-11-08 01:25:05 +0530

    Time: 0 ms (100%), Space: 12 MB (15.84%) - LeetHub

--- 0814-binary-tree-pruning/0814-binary-tree-pruning.cpp
+++ 0814-binary-tree-pruning/0814-binary-tree-pruning.cpp
@@ -6,36 +6,21 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    bool isonepresent(TreeNode* root){
-        if(root==NULL){
-            return false;
-        }
-        
-        return ( (root->val==1)|| isonepresent(root->left) || isonepresent(root->right));
-    }
     TreeNode* pruneTree(TreeNode* root) {
-        if(root==NULL){
+        if (root == NULL) {
             return NULL;
         }
-         root->left = pruneTree(root->left);
-         root->right = pruneTree(root->right);
-         if (root->left==NULL && root->right==NULL && root->val==0){
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+        if (root->left == NULL && root->right==NULL && root->val == 0) {
             return NULL;
-         }
-
-        if(!isonepresent(root->left)){
-            root->left=NULL;
-           
-        }
-         if(!isonepresent(root->right)){
-            root->right = NULL;
         }
         return root;
-        
     }
 };
