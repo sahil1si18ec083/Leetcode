0577650760ededc1cc2e9e7e8fc47df9aabe2ad0
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-11-08 18:43:28 +0530

    Time: 188 ms (7.17%), Space: 85.9 MB (54.95%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
@@ -14,9 +14,7 @@ var minDepth = function(root) {
     if (root==null){
         return 0
     }
-    if (root.left==null && root.right==null){
-        return 1
-    }
+    
     if (root.left==null){
         return (1+minDepth(root.right)) 
     }
