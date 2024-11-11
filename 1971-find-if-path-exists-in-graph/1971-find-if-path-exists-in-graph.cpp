28d462d7e609b2072961c458c0a3a33e84e1aeb7
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-11-11 22:06:09 +0530

    Time: 528 ms (41.64%), Space: 328.5 MB (48.24%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
-    void dfs(int n, int source, int destination, unordered_map<int, vector<int>> &mp,
-             bool& flag, vector<bool>& visited) {
-        if (source == destination) {
-            flag = true;
-            return;
-        }
-        visited[source] = true;
-        for (int i = 0; i < mp[source].size(); i++) {
-            if (visited[mp[source][i]] == false) {
-                dfs(n,mp[source][i], destination, mp, flag, visited);
-            }
-        }
-    }
     bool validPath(int n, vector<vector<int>>& edges, int source,
                    int destination) {
-        unordered_map<int, vector<int>> mp;
-        vector<bool> visited(n, false);
+
+        unordered_map<int, vector<int>> adj;
         for (int i = 0; i < edges.size(); i++) {
             int u = edges[i][0];
             int v = edges[i][1];
-            mp[u].push_back(v);
-            mp[v].push_back(u);
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
-        bool flag = false;
+        vector<bool> visited(n, false);
+        queue<int> q;
+        q.push(source);
+        visited[source] = true;
+        while (q.size() > 0) {
+            int qsize = q.size();
+            int temp = q.front();
+            if (temp == destination) {
+                return true;
+            }
+            q.pop();
 
-        dfs(n, source, destination, mp, flag, visited);
-
-        return flag;
+            for (int i = 0; i < adj[temp].size(); i++) {
+                if (visited[adj[temp][i]] == false) {
+                    q.push(adj[temp][i]);
+                    visited[adj[temp][i]] = true;
+                }
+            }
+        }
+        return false;
     }
 };
