38f77d5bafc2f9f79914212d2b4f604558106292
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-11-11 22:55:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        if(trust.size()==0) return 1;
+
+        unordered_map<int, vector<int>> adj;
+        for (int i = 0; i < trust.size(); i++) {
+            int u = trust[i][0];
+            int v = trust[i][1];
+            adj[u].push_back(v);
+        }
+        int res = -1;
+
+        for (int i = 1; i <= n; i++) {
+            if (adj.find(i) == adj.end()) {
+                res = i;
+            }
+        }
+       
+        for (auto i : adj) {
+            bool check = false;
+            for (int j = 0; j < i.second.size(); j++) {
+                int first = i.first;
+                if (adj[first][j] == res) {
+                    check = true;
+                }
+            }
+            if (check == false) {
+                return -1;
+            }
+        }
+
+        return res;
+    }
+};
