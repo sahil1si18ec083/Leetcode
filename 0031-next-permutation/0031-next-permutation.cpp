eadbdf95b2068e225d6a02f46dee9d0611abae70
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-11-18 03:50:45 +0530

    Time: 0 ms (100%), Space: 14.8 MB (48.8%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
+    void reverse(vector<int>& nums, int i, int j){
+        while(j>i){
+            swap(nums[i], nums[j]);
+            i++;
+            j--;
+        }
+    }
     void nextPermutation(vector<int>& nums) {
-        int dipindex=-1;
+        int gola=-1;
         int n = nums.size();
-        for(int i=n-1;i>=1;i--){
-            if(nums[i-1]<nums[i]){
-                dipindex= i-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                gola = i;
                 break;
             }
-
         }
-        if (dipindex==-1){
-            // just blinding reverse
-            for(int i=0;i<n/2;i++){
-                swap(nums[i], nums[n-1-i]);
-            }
-            return ;
+        if(gola==-1){
+             reverse(nums, 0, n-1);
+             return ;
         }
-        int swapped=dipindex;
-        for(int i=n-1;i>=dipindex+1;i--){
-            if(nums[i]>nums[dipindex]){
-                swapped= i;
+        int swapped =-1;
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]>nums[gola]){
+                swapped = i;
                 break;
-
             }
         }
-        swap(nums[dipindex], nums[swapped]);
-
-        reverse(nums.begin()+dipindex+1, nums.end());
-
-        
+        swap(nums[gola], nums[swapped]);
+         reverse(nums, gola+1, n-1);
         
     }
 };
