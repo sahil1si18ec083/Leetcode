55575e4ce3cf671888c8993bb790f9a85551d8a0
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-11-24 03:36:21 +0530

    Time: 0 ms (100%), Space: 62.4 MB (50.96%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    int peakIndexInMountainArray(vector<int>& nums) {
-        int n = nums.size();
-        if(n==1){
-            return 0;
-        }
-        
-        if (nums[0]>nums[1]){
-            return 0;
-        }
-        if (nums[n-1]>nums[n-2]){
-            return n-1;
-        }
-        int low=1;
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n = arr.size();
+        if(n==1) return 0;
+
+        if(arr[0]>arr[1]) return 0;
+
+        if(arr[n-1]> arr[n-2]) return (n-1);
+
+        int low=0;
         int high = n-1;
         while(low<=high){
-            int mid=(low+high)/2;
-            if((mid==0 || nums[mid]>nums[mid-1]) && (mid==n-1 || nums[mid]>nums[mid+1])){
+            int mid =(low+high)/2;
+
+            if( (mid==0 || arr[mid]>arr[mid-1]) && (mid==n-1 || arr[mid]>arr[mid+1])){
                 return mid;
             }
-            else if ((mid==0 || nums[mid]>nums[mid-1])){
-                low= mid+1;
+            else if ((mid==0 || arr[mid]>arr[mid-1])){
+                low=mid+1;
             }
             else{
                 high = mid-1;
             }
         }
         return -1;
-        
-        
     }
 };
