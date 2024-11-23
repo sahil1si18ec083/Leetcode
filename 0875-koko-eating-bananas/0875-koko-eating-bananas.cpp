5c32ed6e38faae80939f2f7cda1c61edafeef44a
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-11-24 04:46:59 +0530

    Time: 7 ms (76.6%), Space: 21.8 MB (43.99%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
-     long long int sum(vector<int>& piles) {
-        long long int c = 0;
-        for (int i = 0; i < piles.size(); i++) {
-            c += piles[i];
-        }
-        return c;
-    }
-     long long int calculatehrs(vector<int>& piles, long long  int speed){
-         long long int r=0;
+    int calculate(vector<int>& piles,long long int mid){
+        int total =0;
         for(int i=0;i<piles.size();i++){
-            if(piles[i]%speed==0){
-                r+=piles[i]/speed;
+            int rem = piles[i]%mid;
+            int val = piles[i]/mid;
+            if(rem==0){
+                total = total + val;
             }
             else{
-                r+=piles[i]/speed+1;
+                total = total + val+1;
             }
+
         }
-        return r;
+        return total;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        long long int low = 1;
-        long long  int high = sum(piles);
-        long long  int ans=-1;
+        long long int ans=-1;
 
-        while (low <= high) {
-            long long  int mid = (low + high) / 2;
-            if(calculatehrs(piles,mid)<=h){
+       long long int low=1;
+       long long int high=0;
+        for(long long int i=0;i<piles.size();i++){
+            high+=piles[i];
+        }
+        while(low<=high){
+           long long int mid = (low+high)/2;
+
+            if(calculate( piles,mid)<=h){
                 ans = mid;
-                high= mid-1;
+                high = mid-1;
             }
             else{
                 low= mid+1;
             }
         }
+
         return ans;
+        
     }
 };
