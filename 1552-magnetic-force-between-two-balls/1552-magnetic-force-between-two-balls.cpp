a2ced62389c5658374d2a00824dc132ec9a1e5b6
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-12-02 00:17:53 +0530

    Time: 52 ms (46.85%), Space: 61.7 MB (57.06%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
-    bool check(vector<int>& position, int m, int mid) {
-        long long int count = 1;
-        long long int currentpos = position[0];
-        for (long long int i = 1; i < position.size(); i++) {
-            long long int diff = position[i] - currentpos;
-            if (diff >= mid) {
-                count++;
-                currentpos = position[i];
+    bool helper(vector<int>& position, int m, int n, long long int mid) {
+        int balls = 1;
+        long long int pos = position[0];
+
+        for (int i = 1; i < n; i++) {
+            if (position[i] - pos < mid) {
+                // pos = position[i];
+
+            } else {
+                balls++;
+                pos = position[i];
             }
         }
-
-        if (count >= m)
-            return true;
-
-        return false;
+        if (balls < m)
+            return false;
+        return true;
     }
     int maxDistance(vector<int>& position, int m) {
         sort(position.begin(), position.end());
 
-        long long int ans = -1;
         long long int low = 0;
         long long int high = 0;
-        for (long long int i = 0; i < position.size(); i++) {
+        int n = position.size();
+        for (int i = 0; i < n; i++) {
             high += position[i];
         }
+        long long int ans = -1;
+
         while (low <= high) {
             long long int mid = (low + high) / 2;
-
-            if (check(position, m, mid)) {
+           
+            if (helper(position, m, n, mid)) {
                 ans = mid;
                 low = mid + 1;
             } else {
                 high = mid - 1;
             }
         }
-
-        return ans;
+        return (int)ans;
     }
 };
