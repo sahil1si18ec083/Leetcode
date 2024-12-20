8c5f76d59449065d5a0717605a930a30acff4a41
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-12-20 22:52:56 +0530

    Time: 0 ms (100%), Space: 50.6 MB (66.86%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.js
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.js
@@ -1,40 +1,38 @@
-
-
 /**
  * @param {number[]} nums
  * @param {number} target
- * @return {number}
+ * @return {boolean}
  */
-var search = function (nums, target) {
-    const n = nums.length;
+var search = function(nums, target) {
+
     let low = 0;
+    const n = nums.length;
     let high = n - 1;
     while (low <= high) {
         let mid = Math.floor((low + high) / 2);
-        if (nums[mid] == target) {
-            return true;
-        }
-        else if (nums[low]==nums[mid] && nums[mid]==nums[high] && nums[mid]!=target){
+        
+        if (nums[mid] == target) return true;
+        else if(nums[mid]==nums[low] && nums[mid]==nums[high]){
             low++;
             high--;
+            continue;
         }
-        // if left part is sorted
         else if (nums[mid] >= nums[low]) {
-            // if target lies then skip the right part
+            // if left part is sorted
             if (target >= nums[low] && target <= nums[mid]) {
                 high = mid - 1;
+
             }
             else {
-                low = mid + 1
+                low = mid + 1;
             }
-
         }
         else {
-            // if right part is sorted
+            // if right is sorted
             if (target >= nums[mid] && target <= nums[high]) {
-                // if it exits on right side then skip the lft portion
 
-                low = mid + 1
+                low = mid + 1;
+
             }
             else {
                 high = mid - 1;
@@ -42,6 +40,7 @@ var search = function (nums, target) {
             }
         }
     }
+    return false;
 
-    return false
+    
 };
