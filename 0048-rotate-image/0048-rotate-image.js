a0707da50d2f9bb67469ce5a8e72051d0bc926b4
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-12-21 19:51:44 +0530

    Time: 0 ms (100%), Space: 49.3 MB (62.76%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.js
+++ 0048-rotate-image/0048-rotate-image.js
@@ -2,39 +2,32 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-var rotate = function(matrix) {
-    function transpose(matrix){
-        const n = matrix.length;
-        const m = matrix[0].length;
-        for(let i=0;i<n;i++){
-            for (let j=i;j<n;j++){
-                let temp = matrix[i][j];
-                matrix[i][j]= matrix[j][i];
-                matrix[j][i]= temp;
-            }
+function transpose(matrix) {
+    const n = matrix.length;
+    for (let i = 0; i < n; i++) {
+        for (let j = i; j < n; j++) {
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
         }
     }
-    function rotaterow(matrix){
-const n = matrix.length;
-        const m = matrix[0].length;
-        for(let i=0;i<n;i++){
-            let start=0;
-            let end=m-1;
-            while(start<=end){
+}
+var rotate = function (matrix) {
 
-                let temp = matrix[i][start];
-                matrix[i][start]= matrix[i][end];
-                matrix[i][end]= temp;
-                start++;
-                end--;
-
-            }
-        }
-    }
 
     transpose(matrix);
-    rotaterow(matrix);
+    const n = matrix.length;
+    for (let i = 0; i < n; i++) {
+        let start = 0;
+        let end = n - 1;
+        while (start < end) {
+            let temp = matrix[i][start];
+            matrix[i][start] = matrix[i][end];
+            matrix[i][end] = temp;
+            start++;
+            end--;
+        }
+    }
 
-    return matrix;
-    
+
 };
