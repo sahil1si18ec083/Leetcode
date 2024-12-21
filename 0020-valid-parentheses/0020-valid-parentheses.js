86c0373717f195a47627a16d0d01d098682f9149
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-12-22 02:15:49 +0530

    Time: 2 ms (74.39%), Space: 52 MB (21.56%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -2,42 +2,37 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    class Stack {
-    constructor(array) {
-        this.array = array;
+var isValid = function (s) {
+    if (s.length == 0) return true;
+    const stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == ')' || s[i] == ']' || s[i] == '}') {
+            if (stack.length == 0) {
+                return false;
+            }
+            if (s[i] == ")" && stack[stack.length - 1] == "(") {
+                stack.pop();
+            }
+            else if (s[i] == "]" && stack[stack.length - 1] == "[") {
+                stack.pop();
+            }
+            else if (s[i] == "}" && stack[stack.length - 1] == "{") {
+                stack.pop();
+            }
+            else {
+                stack.push(s[i]);
+            }
+        }
+        else {
+            stack.push(s[i]);
+        }
     }
-    push(value) {
-        this?.array?.push(value);
-    }
-    pop() {
-        this?.array?.pop();
-    }
-    get top() {
-        return this.array[this.array.length - 1];
-    }
-    get length() {
-        return this?.array?.length;
-    }
-}
-const myStack = new Stack([]);
-for(let i=0;i<s.length;i++){
-    if (myStack.length==0 && (s[i]==")" || s[i]=="}" || s[i]=="]" )){
-        return false;
-    }
-    if ((s[i]==")" && myStack.top=="(") || (s[i]=="]" && myStack.top=="[") || (s[i]=="}" && myStack.top=="{")  ){
-        myStack.pop()
-    }
-    else{
-        myStack.push(s[i])
-    }
-}
-console.log(myStack)
-if (myStack.length===0){
-    return true;
-}
-else{
-    return false;
-}
 
-}; 
+    if (stack.length == 0) return true;
+
+    return false;
+
+
+
+
+};
