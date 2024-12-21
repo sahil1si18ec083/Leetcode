8c8330371dae79b665efae4a60c82467a5cd1725
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-12-21 16:34:04 +0530

    Time: 0 ms (100%), Space: 55.3 MB (95.82%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -3,23 +3,27 @@
  * @return {number[]}
  */
 var sortedSquares = function (nums) {
-    const n = nums.length;
-    const res = new Array(n).fill(-1);
+
+    let n = nums.length;
+    const res = new Array(n).fill(0);
+
     let low = 0;
     let high = n - 1;
-    let j=n-1;
-    while (low <= high) {
-        if (nums[low] * nums[low] <= nums[high] * nums[high]) {
-            res[j] = nums[high] * nums[high];
-            high--;
 
-        }
-        else {
-            res[j] = nums[low] * nums[low];
+    while (low <= high) {
+        if (nums[low] * nums[low] > nums[high] * nums[high]) {
+            res[n - 1] = nums[low] * nums[low];
             low++;
         }
-        j--;
+        else {
+            res[n - 1] = nums[high] * nums[high];
+            high--;
+        }
+        n--;
     }
-    return res
+
+    return res;
+
+
 
 };
