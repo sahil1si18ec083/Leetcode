b29ba2717f36f01485801a8e2f76c7977d8169b3
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-12-21 17:43:08 +0530

    Time: 4 ms (47.74%), Space: 50.5 MB (86.18%) - LeetHub

--- 0179-largest-number/0179-largest-number.js
+++ 0179-largest-number/0179-largest-number.js
@@ -2,32 +2,33 @@
  * @param {number[]} nums
  * @return {string}
  */
-var largestNumber = function(nums) {
-    nums.sort((a,b)=>{
-       
+var largestNumber = function (nums) {
+
+
+    nums.sort((a, b) => {
         let astring = String(a);
         let bstring = String(b);
-        let ab = Number(astring + bstring);
-        let ba = Number(bstring+ astring);
-        if (ab>ba){
-            return -1
+
+        let poss1 = Number(astring + bstring); // 102
+        let poss2 = Number(bstring + astring); // 210
+
+        if(poss2> poss1){
+            return 1;
         }
-        return 1
-
-        
-    })
-
-    var res=""
-    console.log(nums)
+        else{
+            return -1;
+        }
 
 
+
+    });
+    let res="";
     for(let i=0;i<nums.length;i++){
-        res = res + String(nums[i])
+        res += String(nums[i]);
     }
-    while(res.length>1  && res[0]=="0"){
-        res = res.slice(1)
+    while(res.length>1 && res[0]=='0'){
+        res = res.slice(1);
     }
+    return res;
 
-    return res
-    
 };
