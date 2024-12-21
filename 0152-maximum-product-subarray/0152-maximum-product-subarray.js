eb03c5194d539ae99a525d9921ff17af7a905b04
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-12-21 16:02:32 +0530

    Time: 57 ms (8.34%), Space: 49.6 MB (93.49%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.js
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -2,20 +2,24 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxProduct = function(nums) {
-    let maxProduct=-12345;
-   
-    for (let i=0;i<nums.length;i++){
-                let product=1;
-        for (let j=i;j<nums.length;j++){
-            product=product*nums[j];
-            if(product>maxProduct){
-                maxProduct=product;
-            }
-    
-
+var maxProduct = function (nums) {
+    let prefixProduct = 1;
+    let suffixProd = 1;
+    let max=-1000;
+    const n = nums.length;
+    for (let i = 0; i < n; i++) {
+        if (prefixProduct == 0) {
+            prefixProduct = 1;
         }
+        if (suffixProd == 0) {
+            suffixProd = 1;
+        }
+        prefixProduct = prefixProduct * nums[i];
+        suffixProd = suffixProd * nums[n - 1 - i];
+        
+        max = Math.max(max,prefixProduct, suffixProd )
+
     }
-    return maxProduct;
-    
+    return max
+
 };
