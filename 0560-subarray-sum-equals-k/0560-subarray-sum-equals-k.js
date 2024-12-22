151a340945c371afc37e68182855225a42594ca6
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-12-23 02:28:25 +0530

    Time: 17 ms (73.27%), Space: 57.3 MB (78.02%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -4,24 +4,25 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
-    var count=0;
-    const map = new Map();
-    map.set(0,1);
+    let res=0;
     const n = nums.length;
-    let prefixsum=0;
+
+    const map = new Map();
+    map.set(0, 1);
+    let prefixsum =0;
+
     for(let i=0;i<n;i++){
-        prefixsum = prefixsum + nums[i];
-        if (map.has(prefixsum-k)){
-            count = count + map.get(prefixsum-k);
-        
+        prefixsum+=nums[i];
+        if(map.has(prefixsum-k)){
+            res += map.get(prefixsum-k)
         }
-        if (map.has(prefixsum)){
-            map.set(prefixsum, map.get(prefixsum)+1)
+        if(map.has(prefixsum)){
+            map.set(prefixsum, map.get(prefixsum)+1);
         }
         else{
-            map.set(prefixsum,1)
+             map.set(prefixsum,1);
         }
-
     }
-    return count
+    return res;
+    
 };
