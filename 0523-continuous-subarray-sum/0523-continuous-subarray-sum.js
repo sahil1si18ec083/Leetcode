ace57f6db988c50ea8f46584f504969effaf1362
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-12-23 02:39:16 +0530

    Time: 32 ms (40.45%), Space: 77.1 MB (45.39%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.js
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.js
@@ -4,28 +4,25 @@
  * @return {boolean}
  */
 var checkSubarraySum = function (nums, k) {
-    let prefixsum = 0;
+
+
+    const n = nums.length;
+    if (n < 2) return false;
     const map = new Map();
+
     map.set(0, -1);
+    let prefixsum = 0;
 
-    for (let i = 0; i < nums.length; i++) {
-        
+    for (let i = 0; i < n; i++) {
         prefixsum = prefixsum + nums[i];
-        let res = prefixsum % k;
-
-        if (map.has(res) && i - map.get(res) > 1) {
-           
-
+        let rem = prefixsum % k;
+        if (map.has(rem) && (i - map.get(rem) > 1)) {
             return true;
         }
-        if (!map.has(res)) {
-            map.set(res, i)
-
+        if (!map.has(rem)) {
+            map.set(rem, i);
         }
 
-
-
     }
-    return false
-
+    return false;
 };
