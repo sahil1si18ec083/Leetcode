1fe6ee352648c7cdd045e394df88a9f5bf286241
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-12-23 02:48:12 +0530

    Time: 15 ms (73.63%), Space: 53.4 MB (98.9%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.js
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.js
@@ -4,26 +4,34 @@
  * @return {number}
  */
 var subarraysDivByK = function (nums, k) {
-    const n = nums.length;
-    let prefsum = 0;
-    let count = 0;
     const map = new Map();
-    map.set(0, 1)
+
+    const n = nums.length;
+
+    map.set(0, 1);
+    let res = 0;
+
+    let prefixsum = 0;
+
     for (let i = 0; i < n; i++) {
-        prefsum = prefsum + nums[i];
-        let res = prefsum % k;
-        if (res < 0) {
-            res = res + k;
+        prefixsum += nums[i];
+
+        let rem = prefixsum % k;
+        if(rem<0){
+            rem = rem + k;
         }
 
-        if (map.has(res)) {
-            count = count + map.get(res);
-            map.set(res, map.get(res) + 1)
+        if (map.has(rem)) {
+            res = res + map.get(rem);
+            map.set(rem, map.get(rem) + 1);
+
         }
         else {
-            map.set(res, 1)
+
+            map.set(rem, 1);
         }
+
     }
-    return count
+    return res;
 
 };
