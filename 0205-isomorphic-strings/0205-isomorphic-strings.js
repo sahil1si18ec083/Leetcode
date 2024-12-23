3f31a669fc169abaa706a880cae43f28fce8a44f
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-12-23 22:42:16 +0530

    Time: 12 ms (20.28%), Space: 52 MB (28.9%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -3,36 +3,39 @@
  * @param {string} t
  * @return {boolean}
  */
-var isIsomorphic = function(s, t) {
-     if (s.length!=t.length){
-            return false;
-        }
-        const map = new Map();
-        for(let i=0;i<s.length;i++){
-            if(map.has(s[i])){
-                if(map.get(s[i])!=t[i]){
-                    return false
-                }
-            }
-            else{
-                map.set(s[i], t[i])
-            }
-        }
-        map.forEach((value,key)=>{
-            map.delete(key)
-        })
+var isIsomorphic = function (s, t) {
+    const map = new Map();
 
-        for(let i=0;i<t.length;i++){
-            if(map.has(t[i])){
-                if(map.get(t[i])!=s[i]){
-                    return false
-                }
-            }
-            else{
-                map.set(t[i], s[i])
-            }
-        }
- 
+    let n = s.length;
+    let m = t.length;
+    if (m != n) return false;
 
-    return true
+    for (let i = 0; i < n; i++) {
+        if (map.has(s[i])) {
+            if (map.get(s[i]) != t[i]) {
+                return false;
+            }
+
+        }
+        else {
+            map.set(s[i], t[i]);
+        }
+    }
+    map.forEach((value, key) => {
+        map.delete(key)
+    })
+    for (let i = 0; i < n; i++) {
+        if (map.has(t[i])) {
+            if (map.get(t[i]) != s[i]) {
+                return false;
+            }
+
+        }
+        else {
+            map.set(t[i], s[i]);
+        }
+    }
+
+    return true;
+
 };
