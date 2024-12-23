01aec387b0a0c04c1ad95e8c3b230b61fded505b
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-12-23 22:37:34 +0530

    Time: 0 ms (100%), Space: 50.6 MB (80.93%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let xor=nums[0];
+    for(let i=1;i<nums.length;i++){
+        xor = xor ^ nums[i];
+    }
+    
+    return xor;
+};
