33a30dbf602b17bd95f3ae8cfc1cd1bce8630c5a
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-12-27 00:26:01 +0530

    Time: 456 ms (45.58%), Space: 329 MB (50.22%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -4,26 +4,23 @@ public:
                    int destination) {
 
         unordered_map<int, vector<int>> adj;
+        vector<bool> visited(n, false);
+
         for (int i = 0; i < edges.size(); i++) {
             int u = edges[i][0];
             int v = edges[i][1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        vector<bool> visited(n, false);
         queue<int> q;
         q.push(source);
         visited[source] = true;
         while (q.size() > 0) {
-            int qsize = q.size();
             int temp = q.front();
-            if (temp == destination) {
-                return true;
-            }
             q.pop();
-
+            if(temp == destination) return true;
             for (int i = 0; i < adj[temp].size(); i++) {
-                if (visited[adj[temp][i]] == false) {
+                if( visited[adj[temp][i]] == false){
                     q.push(adj[temp][i]);
                     visited[adj[temp][i]] = true;
                 }
