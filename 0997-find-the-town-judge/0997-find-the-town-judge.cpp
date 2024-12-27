a673239a13aacf08c9e27002cd2e1f06574b80b5
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-12-27 15:12:19 +0530

    Time: 0 ms (100%), Space: 65 MB (77.49%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -2,19 +2,23 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
 
-        vector<int>balance(n+1, 0);
-        for(int i=0;i<trust.size();i++){
-            int u= trust[i][0];
-            int v = trust[i][1];
-            balance[v]++;
-            balance[u]--;
+        vector<int> trustrelation(n, 0);
+
+        for (int i = 0; i < trust.size(); i++) {
+            int u = trust[i][0] - 1;
+
+            int v = trust[i][1] - 1;
+
+            trustrelation[v]++;
+
+            trustrelation[u]--;
         }
-        for(int i=1;i<=n;i++){
-            if (balance[i]==n-1){
-                return i;
+
+        for (int i = 0; i < n; i++) {
+            if (trustrelation[i] == n - 1) {
+                return i + 1;
             }
         }
         return -1;
-        
     }
 };
