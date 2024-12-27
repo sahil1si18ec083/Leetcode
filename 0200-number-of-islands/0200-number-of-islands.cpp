c3481b19dfc3a11ffa6ab9d446f68b1d7c1e6f92
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-12-27 16:34:36 +0530

    Time: 26 ms (70.04%), Space: 16.3 MB (63.36%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid, int& totalislands, int i, int j, int n,
-             int m) {
-
-        if(i<0 || j<0 || i>=n || j>=m){
-            return ;
-        }        
-        if (grid[i][j] == '0') {
+    void dfs(int i, int j, int m, int n, int& total,
+             vector<vector<char>>& grid) {
+        if (i < 0 || i >= n) {
             return;
         }
-        
-        grid[i][j] = '0';
-        // call four calls in four directions
+        if (j < 0 || j >= m) {
+            return;
+        }
 
-        dfs(grid, totalislands, i - 1, j, n, m);
-        dfs(grid, totalislands, i + 1, j, n, m);
-        dfs(grid, totalislands, i, j - 1, n, m);
-        dfs(grid, totalislands, i, j + 1, n, m);
+        if (grid[i][j] =='0') {
+            return;
+        }
+
+        grid[i][j] = '0';
+
+        dfs(i-1, j, m, n, total,grid );
+        dfs(i+1, j, m, n, total,grid );
+        dfs(i, j-1, m, n, total,grid );
+        dfs(i, j+1, m, n, total,grid );
+
     }
     int numIslands(vector<vector<char>>& grid) {
-
-        int totalislands = 0;
-        int n = grid.size();    // n=4
-        int m = grid[0].size(); // m=5
+        int m = grid[0].size();    // m=4
+        int n = grid.size(); // n=4
+        int total = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if(grid[i][j]=='1'){
-                    totalislands++;
 
-                    dfs(grid, totalislands, i, j, n, m);
-
+                if (grid[i][j] == '1') {
+                    dfs(i, j, m, n, total, grid);
+                    total++;
                 }
-                
             }
         }
-
-        return totalislands;
+        return total;
     }
 };
