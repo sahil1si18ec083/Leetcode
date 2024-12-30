1580ee61a3bb056d0f290ff9b05eba007d87f9c7
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-12-30 17:00:01 +0530

    Time: 67 ms (31.05%), Space: 34.9 MB (24.7%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,43 +1,44 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        vector<vector<int>> res;
-        int i = 0;
-        while (i < n - 2) {
-            int low = i + 1;
-            int high = n - 1;
-            while (low < high) {
-                
-                if (nums[low] + nums[high] + nums[i] == 0) {
-                    vector<int> temp;
-                    temp.push_back(nums[i]);
-                    temp.push_back(nums[low]);
-                    temp.push_back(nums[high]);
-                    res.push_back(temp);
 
-                    
-                    while (low < n - 2 && nums[low] == nums[low + 1]) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+
+        int low = 0;
+        vector<vector<int>> v;
+
+        while (low < n - 2) {
+            int i = low + 1;
+            int j = n - 1;
+
+            while (j > i) {
+                if (nums[low] + nums[i] + nums[j] == 0) {
+                    vector<int> temp;
+                    temp.push_back(nums[low]);
+                    temp.push_back(nums[i]);
+                    temp.push_back(nums[j]);
+                    v.push_back(temp);
+                    while (low < n - 1 && nums[low] == nums[low + 1]) {
                         low++;
                     }
-                    while (high > 0 && nums[high] == nums[high - 1]) {
-                        high--;
-                    }
-                    while (i < n - 3 && nums[i] == nums[i + 1]) {
+
+                    while (i < n - 1 && nums[i] == nums[i + 1]) {
                         i++;
                     }
-                    low++;
-                    high--;
-                } else if (nums[low] + nums[high] + nums[i] < 0) {
-                    low++;
+                    while (j >= 1 && nums[j] == nums[j - 1]) {
+                        j--;
+                    }
+                    i++;
+                    j--;
+                } else if (nums[low] + nums[i] + nums[j] < 0) {
+                    i++;
                 } else {
-                    high--;
+                    j--;
                 }
             }
-            i++;
+            low++;
         }
-
-        return res;
+        return v;
     }
 };
