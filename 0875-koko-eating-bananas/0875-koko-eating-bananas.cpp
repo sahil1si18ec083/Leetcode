455c13a4057f78f3ac0f88ea17f4d8d3fc62cc58
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2025-01-05 17:14:58 +0530

    Time: 7 ms (76.6%), Space: 22.9 MB (51.69%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
-    int calculate(vector<int>& piles,long long int mid){
-        int total =0;
-        for(int i=0;i<piles.size();i++){
-            int rem = piles[i]%mid;
-            int val = piles[i]/mid;
-            if(rem==0){
-                total = total + val;
+    bool helper(vector<int>& piles, long long int n, long long int mid, int h) {
+        long long int total = 0;
+        for (int i = 0; i < n; i++) {
+            int rem = piles[i] % mid;
+            if (rem != 0) {
+                total++;
             }
-            else{
-                total = total + val+1;
-            }
-
+            total = total + piles[i] / mid;
         }
-        return total;
+        if (total <= h)
+            return true;
+        return false;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        long long int ans=-1;
-
-       long long int low=1;
-       long long int high=0;
-        for(long long int i=0;i<piles.size();i++){
-            high+=piles[i];
+        long long int low = 1;
+        long long int high = 0;
+        long long int ans = -1;
+        long long int n = piles.size();
+        for (long long int i = 0; i < n; i++) {
+            high += piles[i];
         }
-        while(low<=high){
-           long long int mid = (low+high)/2;
-
-            if(calculate( piles,mid)<=h){
+        while (low <= high) {
+            long long int mid = (low + high) / 2;
+            if (helper(piles, n, mid, h)) {
                 ans = mid;
-                high = mid-1;
-            }
-            else{
-                low= mid+1;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
-
         return ans;
-        
     }
 };
