dfed7599e6f632a4e083bc61529e0e5f2e92f198
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2025-01-13 02:09:17 +0530

    Time: 0 ms (100%), Space: 95.3 MB (47.84%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,41 +1,42 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-
-        
-        if(nums2.size()<nums1.size()){
+        int m = nums1.size();
+        int n = nums2.size();
+        if(m>n){
             return findMedianSortedArrays(nums2, nums1);
         }
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        int n = (n1 + n2);
+        int total = m + n;
+
         int low = 0;
-        int high = n1;
-        double val =0.00000;
+        int high = m;
+
         while (low <= high) {
             int mid = (low + high) / 2;
-            int x = (n + 1) / 2 - mid;
-            int l1 = mid ==0 ? INT_MIN : nums1[mid - 1];
-            int l2 = x ==0 ? INT_MIN : nums2[x - 1];
-            int r1 = mid == n1 ? INT_MAX : nums1[mid];
-            int r2 = x == n2 ? INT_MAX : nums2[x];
-            if (l1 <= r2 && l2 <= r1) {
-                if (n % 2 == 0) {
-                    int maxleft = max(l1,l2);
-                    int minright = min(r1,r2);
-                    return (double(maxleft+minright)/2);
+            int x = (m + n + 1) / 2 - mid;
+            int l1 = (mid < 1 ? INT_MIN : nums1[mid - 1]);
+            int l2 = (x < 1 ? INT_MIN : nums2[x - 1]);
+            int r1 = (mid >= m ? INT_MAX : nums1[mid]);
+            int r2 = (x >= n ? INT_MAX : nums2[x]);
+            if (r1 >= l2 && r2 >= l1) {
+                if (total %2==1){
+                    int val = max(l1,l2);
+                    return(double) (val);
 
-                } else {
-                     int maxleft = max(l1,l2);
-                     return double(maxleft);
+                }
+                else{
+                    int sum = max(l1,l2 ) + min(r1, r2);
+                    return ((double(sum)/ double(2)));
+
+
                 }
 
-            } else if (l1 >= r2) {
-                high = mid - 1;
-            } else {
+            } else if (l1 < r2) {
                 low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
-        return val;
+        return 0.00000;
     }
 };
