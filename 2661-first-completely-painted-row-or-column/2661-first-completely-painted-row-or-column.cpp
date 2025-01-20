6722a9c1e3ed4e84b99649afa35c349eb3269ba9
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2025-01-20 13:47:53 +0530

    Time: 418 ms (5.18%), Space: 253.2 MB (6.1%) - LeetHub

--- 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cpp
+++ 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cpp
@@ -5,37 +5,33 @@ public:
         // let me first create mapping of value in mat vs the x and y coordinate
 
         int n = mat.size();
+        int m = mat[0].size();
         unordered_map<int, vector<int>>
             mp; // value in mat vs the x and y coordinate
+            vector<int> rows(n,0);
+            vector<int> columns(m,0);
+
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < m;j++) {
                 vector<int> temp;
                 int  val = mat[i][j];
-
                 temp.push_back(i);
                 temp.push_back(j);
-
                 mp[val] = temp;
-
-
             }
         }
-        unordered_map<int, int> count;
         for(int i=0;i<arr.size();i++){
             int val = arr[i];
             vector<int> temp = mp[val];
             int x = temp[0];
             int y = temp[1];
-            cout<<x<<" "<<y <<endl;
 
-            count[x]++;
-            count[y+n]++;
-            if(count[x]==n ||   count[y+n]==n){
+            rows[x]++;
+            columns[y]++;
+            if(rows[x]==m ||   columns[y]==n){
                 return i;
             }
-            
-
 
         }
         return -1;
