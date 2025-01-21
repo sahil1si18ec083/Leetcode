a81ae8082e13c134bb7fb77daa641e86a67b4e2b
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2025-01-22 02:08:14 +0530

    Time: 209 ms (7.62%), Space: 9.7 MB (87.25%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,24 +1,48 @@
 class Solution {
 public:
+    bool isalpha(char c) {
+        int val = c;
+        if (val >= 65 && val <= 90)
+            return true;
+        if (val >= 97 && val <= 122)
+            return true;
+        if (val >= 48 && val <= 57)
+            return true;
+        return false;
+    }
     bool isPalindrome(string s) {
+        int n = s.size();
         int i = 0;
-        int j = s.size() - 1;
+        int j = n - 1;
         while (j > i) {
-            if (isalnum(s[i]) && isalnum(s[j])) {
-                if (tolower(s[i]) == tolower(s[j])) {
+            if (isalpha(s[i]) && isalpha(s[j])) {
+                int a = s[i];
+                int b = s[j];
+                int diff = b - a;
+                cout<<a<<" a"<<b<<" b"<<endl;
+                if(a==b){
+                    i++;
+                    j--;
+                }
+                else if (((a>=48 && a<=57)|| (b>=48 && b<=57))){
+                    return false;
+                }
+               else  if (diff == 32 || diff == -32 || diff == 0) {
                     i++;
                     j--;
                 } else {
                     return false;
                 }
-            } else if (isalnum(s[i])) {
+            } else if (isalpha(s[i])) {
                 j--;
-            } else if (isalnum(s[j])) {
+            } else if (isalpha(s[j])) {
                 i++;
             } else {
+                i++;
                 j--;
             }
         }
+
         return true;
     }
 };
