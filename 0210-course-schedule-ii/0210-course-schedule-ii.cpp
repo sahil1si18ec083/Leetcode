69f926915232f68a43b4ca23dea4cadb80f299e9
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2025-01-21 21:45:32 +0530

    Time: 4 ms (60.7%), Space: 18.5 MB (52.64%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,51 +1,39 @@
 class Solution {
 public:
-    bool iscycle(vector<bool>& visited, unordered_map<int, vector<int>>& adj,
-                 vector<bool>& inrecursionvisited, int n, int i,
-                 stack<int>& mystack) {
-        visited[i] = true;
-        inrecursionvisited[i] = true;
-        for (auto j : adj[i]) {
-            if (visited[j] == false &&
-                iscycle(visited, adj, inrecursionvisited, n, j, mystack)) {
-
-                return true;
-            } else if (visited[j] && inrecursionvisited[j]) {
-
-                return true;
-            }
-        }
-        mystack.push(i);
-        inrecursionvisited[i] = false;
-        return false;
-    }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, vector<int>> mp;
         int n = numCourses;
-        vector<int> emptyresult;
-        vector<int> result;
-        stack<int> mystack;
-        vector<bool> visited(n, false);
-        vector<bool> inrecursionvisited(n, false);
-        unordered_map<int, vector<int>> adj;
+        vector<int> indegree(n, 0);
+        queue<int> q;
+        vector<int> res;
+
         for (int i = 0; i < prerequisites.size(); i++) {
             int u = prerequisites[i][0];
             int v = prerequisites[i][1];
-            adj[v].push_back(u);
+            mp[v].push_back(u);
+
+            indegree[u]++;
         }
-        for (int i = 0; i < n; i++) {
-            if (visited[i] == true) {
-                continue;
-            }
-            if (visited[i] == false &&
-                iscycle(visited, adj, inrecursionvisited, n, i, mystack)) {
-                return emptyresult;
+        for (int i = 0; i < indegree.size(); i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
             }
         }
-        cout << mystack.size() << endl;
-        while (mystack.size() > 0) {
-            result.push_back(mystack.top());
-            mystack.pop();
+        while (q.size() > 0) {
+            int front = q.front();
+            q.pop();
+            res.push_back(front);
+            for (auto j : mp[front]) {
+                indegree[j]--;
+                if (indegree[j] == 0) {
+                    q.push(j);
+                }
+            }
         }
-        return result;
+
+        if (res.size() == n)
+            return res;
+
+        return {};
     }
 };
