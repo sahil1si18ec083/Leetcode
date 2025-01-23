699f96a122a20063945eefcb25ae66370ba1a516
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2025-01-24 02:16:35 +0530

    Time: 0 ms (100%), Space: 17.1 MB (41.32%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,77 +1,49 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-
+        int n = grid.size();
+        int m = grid[0].size();
         queue<pair<int, int>> q;
-        int freshcount = 0;
-        int res = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        int fresh = 0;
+        int time = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 2) {
                     q.push({i, j});
-                } else if (grid[i][j] == 1) {
-                    freshcount++;
+                }
+                else if (grid[i][j]==1){
+                    fresh++;
                 }
             }
         }
-        if(freshcount==0) return 0;
-        while (q.size() > 0) {
-            bool rotted= false;
-
-            pair<int, int> front = q.front();
-            q.pop();
-            int x = front.first;
-            int y = front.second;
-            cout << x << "x " << y << " y" << endl;
-
-            if (x - 1 < 0 || grid[x - 1][y] == 0 || grid[x - 1][y] == 2) {
-
-            } else {
-                // cout << "a" << endl;
-                grid[x - 1][y] = 2;
-                freshcount--;
-                q.push({x - 1, y});
-               rotted= true;
+        if (fresh == 0)
+            return 0;
+        vector<vector<int>> directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+        while (!q.empty()) {
+            int size = q.size();
+            bool visited = false;
+            
+            for (int i = 0; i < size; i++) {
+                pair<int, int> front = q.front();
+                q.pop();
+                for (int j = 0; j < directions.size(); j++) {
+                    int x = front.first + directions[j][0];
+                    int y = front.second + directions[j][1];
+                    if (x >= 0 && y >= 0 && x < n && y < m && grid[x][y] != 0 &&
+                        grid[x][y] != 2) {
+                        visited = true;
+                        grid[x][y] = 2;
+                        q.push({x, y});
+                        fresh--;
+                    }
+                }
             }
-            if (y - 1 < 0 || grid[x][y - 1] == 0 || grid[x][y - 1] == 2) {
-
-            } else {
-                grid[x][y - 1] = 2;
-                freshcount--;
-                q.push({x, y - 1});
-                 rotted= true;
-            }
-
-            if (x + 1 >= m || grid[x + 1][y] == 0 || grid[x + 1][y] == 2) {
-
-            } else {
-                // cout << "a" << endl;
-                grid[x + 1][y] = 2;
-                freshcount--;
-                q.push({x + 1, y});
-                rotted= true;
-            }
-
-            if (y + 1 >= n || grid[x][y + 1] == 0 || grid[x][y + 1] == 2) {
-
-            } else {
-               
-                grid[x][y + 1] = 2;
-                freshcount--;
-                q.push({x, y + 1});
-                 rotted= true;
-            }
-            if( rotted){
-                res++;
+            if(visited){
+                time++;
             }
         }
-       
-
-        if (freshcount == 0)
-            return res-1;
-
-        return -1;
+        if (fresh != 0)
+            return -1;
+        return time;
     }
 };
