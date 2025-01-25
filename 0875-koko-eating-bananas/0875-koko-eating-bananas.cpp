5c6ad11a8605b3705e059092ab83cc381440b490
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2025-01-25 22:54:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    bool helper(vector<int>& piles, long long int n, long long int mid, int h) {
+    long long int calculate(vector<int>& piles, long long int h,
+                            long long int mid) {
         long long int total = 0;
-        for (int i = 0; i < n; i++) {
-            int rem = piles[i] % mid;
+        for (int i = 0; i < piles.size(); i++) {
+            long long int rem = piles[i] % mid;
             if (rem != 0) {
                 total++;
             }
-            total = total + piles[i] / mid;
+            total = total + (piles[i] / mid);
         }
         if (total <= h)
             return true;
         return false;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        long long int low = 1;
+        long long int low = 0;
         long long int high = 0;
-        long long int ans = -1;
-        long long int n = piles.size();
-        for (long long int i = 0; i < n; i++) {
-            high += piles[i];
+
+        for (int i = 0; i < piles.size(); i++) {
+            
+            high = high + piles[i];
         }
+        long long int ans = -1;
         while (low <= high) {
             long long int mid = (low + high) / 2;
-            if (helper(piles, n, mid, h)) {
+            cout <<low<< mid << endl;
+            if (calculate(piles, h, mid)) {
                 ans = mid;
                 high = mid - 1;
             } else {
