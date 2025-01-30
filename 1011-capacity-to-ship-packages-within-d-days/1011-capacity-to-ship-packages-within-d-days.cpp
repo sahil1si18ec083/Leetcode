29ab7304bf46e33f55177d35786f626938907093
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2025-01-30 11:43:43 +0530

    Time: 11 ms (70.74%), Space: 35.1 MB (37.01%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,44 +1,46 @@
 class Solution {
 public:
-    bool possible(vector<int>& weights, int mid, int days) {
+    bool check(int mid, vector<int>& weights, int days, int n){
         int currentweight=0;
-        int dayscount=1;
-        for (int i = 0; i < weights.size(); i++) {
-            if(weights[i]>mid){
-                return false;
-            }
-            if(currentweight+weights[i] <= mid){
+        int calculateddays =1;
+        for(int i=0;i<n;i++){
+            if(weights[i]>mid) return false;
+            if(currentweight+ weights[i]<= mid){
                 currentweight+=weights[i];
             }
             else{
-                dayscount++;
-                currentweight=weights[i];
+                 currentweight=weights[i];
+                 calculateddays++;
+
             }
-
         }
-        if(dayscount<=days) return true;
-
+        if(calculateddays<=days) return true;
         return false;
     }
     int shipWithinDays(vector<int>& weights, int days) {
+        int low = weights[0];
+        int high =0;
+        int n = weights.size();
+        int ans =-1;
 
-        int low = 1;
-        int high = 0;
-        int ans=-1;
-        for (int i = 0; i < weights.size(); i++) {
-            high += weights[i];
+        for(int i=0;i<n;i++){
+            high+= weights[i];
+            low = max(low, weights[i]);
         }
-        while (low <= high) {
-            int mid = (low + high)/2;
 
-            if (possible(weights, mid,days)) {
+        while(low<=high){
+            int mid = (low)+ (high-low)/2;
+
+            if(check(mid, weights, days,n)){
                 ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+                high = mid-1;
+            }
+            else{
+                low  = mid+1;
             }
         }
 
         return ans;
+        
     }
 };
