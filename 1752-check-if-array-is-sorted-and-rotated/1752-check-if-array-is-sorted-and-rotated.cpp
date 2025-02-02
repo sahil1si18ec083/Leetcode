caf4837b878fb49dc3c3c0b5b14924baa9b410a1
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2025-02-02 16:21:16 +0530

    Time: 0 ms (100%), Space: 11.3 MB (22.96%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,17 +1,48 @@
 class Solution {
 public:
+    bool comparetwoArrays(vector<int>& a, vector<int>& b) {
+        int n = a.size();
+        int m = b.size();
+        if (n != m)
+            return false;
+        for (int i = 0; i < n; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    void rotateRight(vector<int>& arr) {
+        int n = arr.size();
+        int val = arr[n - 1];
+        for (int i = n - 1; i >= 1; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = val;
+    }
     bool check(vector<int>& nums) {
 
         int n = nums.size();
-        int peakcount=0;
-        for(int i=0;i<n-1;i++){
-            if (nums[i+1]<nums[i]){
-                peakcount++;
+        vector<int> sortednums;
+        for (int i = 0; i < n; i++) {
+            sortednums.push_back(nums[i]);
+        }
+        sort(sortednums.begin(), sortednums.end());
+
+        for (int i = 0; i < n; i++) {
+            bool flag = true;
+
+            for (int j = 0; j < n; j++) {
+                if(nums[j]!=sortednums[(j+i)%n] ){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag == true){
+                return true;
             }
         }
-        if(peakcount==0) return true;
-        if(peakcount==1 && nums[n-1]<= nums[0]) return true;
+
         return false;
-        
     }
 };
