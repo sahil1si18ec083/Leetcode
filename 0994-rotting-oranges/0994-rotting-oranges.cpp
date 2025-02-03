e54294ef0386716869812f8680a4563f108af8de
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2025-02-03 15:20:22 +0530

    Time: 14 ms (5.17%), Space: 20.8 MB (5.35%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,49 +1,52 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+
+        int minutes = 0;
         int n = grid.size();
         int m = grid[0].size();
         queue<pair<int, int>> q;
-        int fresh = 0;
-        int time = 0;
+        int freshoaranges =0;
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 2) {
                     q.push({i, j});
                 }
-                else if (grid[i][j]==1){
-                    fresh++;
+                if(grid[i][j]==1){
+                    freshoaranges++;
                 }
             }
         }
-        if (fresh == 0)
-            return 0;
-        vector<vector<int>> directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-        while (!q.empty()) {
+        if(freshoaranges==0) return 0;
+        while (q.size() > 0) {
             int size = q.size();
-            bool visited = false;
-            
-            for (int i = 0; i < size; i++) {
+            bool rottened= false;
+
+            for (int k = 0; k < size; k++) {
                 pair<int, int> front = q.front();
                 q.pop();
-                for (int j = 0; j < directions.size(); j++) {
-                    int x = front.first + directions[j][0];
-                    int y = front.second + directions[j][1];
-                    if (x >= 0 && y >= 0 && x < n && y < m && grid[x][y] != 0 &&
-                        grid[x][y] != 2) {
-                        visited = true;
+                int i = front.first;
+                int j = front.second;
+                vector<vector<int>> directions = {
+                    {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+
+                for (auto it : directions) {
+                    int x = i + it[0];
+                    int y = j + it[1];
+                    if (x >= 0 && y >= 0 && x < n && y < m && grid[x][y] == 1) {
                         grid[x][y] = 2;
                         q.push({x, y});
-                        fresh--;
+                        rottened= true;
+                        freshoaranges--;
                     }
                 }
             }
-            if(visited){
-                time++;
+            if(rottened){
+                minutes++;
             }
         }
-        if (fresh != 0)
-            return -1;
-        return time;
+        if(freshoaranges>0) return -1;
+        return minutes;
     }
 };
