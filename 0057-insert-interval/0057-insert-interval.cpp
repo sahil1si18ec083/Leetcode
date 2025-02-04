98ff51a0ee4c7ec73c7cc5a22de8ffd79ef17fcc
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2025-02-04 16:59:32 +0530

    Time: 212 ms (5.25%), Space: 61.5 MB (5.27%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,38 +1,24 @@
 class Solution {
 public:
-    static bool mycomparator(vector<int>& a, vector<int>& b) {
-        return a[0] < b[0];
-    }
+    static bool comparator(vector<int> a, vector<int> b) { return a[0] < b[0]; }
     vector<vector<int>> insert(vector<vector<int>>& intervals,
                                vector<int>& newInterval) {
 
-        vector<vector<int>> res;
-
         intervals.push_back(newInterval);
-
-        sort(intervals.begin(), intervals.end(), mycomparator);
-
+        vector<vector<int>> result;
+        sort(intervals.begin(), intervals.end(), comparator);
+        result.push_back(intervals[0]);
         int n = intervals.size();
-        if (n < 2) {
-            return intervals;
-        }
-
-        res.push_back(intervals[0]);
-
         for (int i = 1; i < n; i++) {
-            int firstval = res[res.size() - 1][0];
-            int secondval = res[res.size() - 1][1];
-
-            if (intervals[i][0] > secondval) {
-                res.push_back(intervals[i]);
+            int s = intervals[i][0];
+            int e = intervals[i][1];
+            if (s > result[result.size() - 1][1]) {
+                result.push_back({s, e});
             } else {
-                res.pop_back();
-                vector<int> temp;
-                temp.push_back(firstval);
-                temp.push_back(max(secondval, intervals[i][1]));
-                res.push_back(temp);
+                result[result.size() - 1][1] =
+                    max( result[result.size() - 1][1], e);
             }
         }
-        return res;
+        return result;
     }
 };
