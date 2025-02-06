2e21c0ab43837c7bdc8587bf424b9c2ab715cfbf
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2025-02-06 20:40:14 +0530

    Time: 0 ms (100%), Space: 15.2 MB (62.48%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -6,28 +6,28 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    void solve(TreeNode* root, vector<int>&v){
-        if(root==NULL){
-            return ;
-        }
-        if(root->left==NULL && root->right==NULL){
+    void dfs(vector<int>& v, TreeNode* root) {
+        if (root == NULL)
+            return;
+        if(!root->left && !root->right){
             v.push_back(root->val);
-        }
-        solve(root->left, v);
-        solve(root->right, v);
+        }    
+        dfs(v, root->left);
+        dfs(v, root->right);
     }
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector<int>v1;
-        vector<int>v2;
-        solve(root1, v1);
-        solve(root2, v2);
-        return v1==v2;
 
-        
+        vector<int> v1;
+        vector<int> v2;
+
+        dfs(v1, root1);
+        dfs(v2, root2);
+        return (v1 == v2);
     }
 };
